5d981e8948eb99dc1af57913be3e8953b0ff9cbe
Timofey Kozlov <39344326+Tlmonko@users.noreply.github.com>
2020-11-29 12:23:14 +0500

    scripts created

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class ColorfulWall : MonoBehaviour
+{
+    void OnTriggerEnter(Collider collider)
+    {
+        var wall = collider.gameObject.GetComponent<Renderer>();
+        wall.material.color = Random.ColorHSV();
+    }
+}
---
+++ Assets/Scripts/CubePush.cs
@@ -0,0 +1,17 @@
+﻿using Packages.Rider.Editor.Util;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    public Rigidbody СubeRB;
+    public Vector3 vector;
+    public float acceleration;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            СubeRB.AddForce(vector.normalized * acceleration, ForceMode.Impulse);
+        }
+    }
+}
